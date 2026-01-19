561eb926b356af4a2739425348f1bda1e2d75695
Gustavo Gil <5755070+diemort@users.noreply.github.com>
2026-01-19 16:35:23 -0300

    adding options related to the kinematics of tracks

--- PhysicsTools/NanoAOD/python/dileptonTrackMultiplicity_cfi.py
+++ PhysicsTools/NanoAOD/python/dileptonTrackMultiplicity_cfi.py
@@ -3,16 +3,20 @@ import FWCore.ParameterSet.Config as cms
 dileptonTrackMultiplicity = cms.EDProducer(
     "DileptonTrackMultiplicityProducer",
 
-    leptonType = cms.string("muon"),  # "muon" or "electron"
+    leptonType = cms.string("muon"),
 
     muons     = cms.InputTag("slimmedMuons"),
     electrons = cms.InputTag("slimmedElectrons"),
     tracks    = cms.InputTag("packedPFCandidates"),
     vertices  = cms.InputTag("offlineSlimmedPrimaryVertices"),
 
-    maxDz = cms.double(0.5),  # cm
     minPt = cms.double(0.4),
+    maxDz = cms.double(0.5),
+    maxDxy = cms.double(0.2),
 
-    # --- verbosity control (0 = silent, >=1 = log output)
-    verbose = cms.untracked.int32(0),
+    requireHighPurity = cms.bool(True),
+    minTrackerLayers = cms.int32(6),
+    maxChi2 = cms.double(5.0),
+
+    verbose = cms.untracked.int32(0)
 )
